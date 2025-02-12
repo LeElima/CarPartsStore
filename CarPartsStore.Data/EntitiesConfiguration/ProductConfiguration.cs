eeb1f2be27ad0bba3d9d5e368506a4b3e1d331a5
LeElima <leandrolimaa2008@hotmail.com>
2025-02-12 08:42:58 -0300

    Configuração inicial de injeção de dependencia e configuração do context

---
+++ CarPartsStore.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarPartsStore.Domain.Entities;
+
+namespace CarPartsStore.Data.EntitiesConfiguration
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Price).HasPrecision(10, 2);
+            //builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
---
+++ CarPartsStore.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPartsStore.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+    }
+}
