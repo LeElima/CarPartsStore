0aae1f8eadd8d1fed7626c8b9df0e00643229f3f
LeElima <leandrolimaa2008@hotmail.com>
2025-02-17 07:34:51 -0300

    Atualizado pacotes

--- CarPartsStore.Data/Context/CarPartsStoreContext.cs
+++ CarPartsStore.Data/Context/CarPartsStoreContext.cs
@@ -14,6 +14,7 @@ namespace CarPartsStore.Data.Context
         { }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
